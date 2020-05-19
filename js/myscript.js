7348fdd6a2b9590ac480b10d3b8d80f56764e846
Giuseppe Rotolo <rotolo37@gmail.com>
2020-05-19 13:58:19 +0200

    Password Generator created

---
+++ js/myscript.js
@@ -0,0 +1,11 @@
+// name user
+var nameUser = prompt('What is your name?');
+
+// suername user
+var surnameUser = prompt('What is your surname?');
+
+// favourite color user
+var colorUser = prompt('What is your favourite color?');
+
+
+document.getElementById('password').innerHTML = (nameUser + surnameUser + colorUser + 19);
